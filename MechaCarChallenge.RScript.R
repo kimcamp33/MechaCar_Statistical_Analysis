16291bda83c3e7bac6b95737cc586f196cca6858
Kimberly Campbell-Smith <kimberlycampbell-smith@Kimberlys-MacBook-Air.local>
2022-07-09 23:23:34 -0400

    deliverable 1

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,7 +2,7 @@
 library("dplyr")
 
 ## Import and read in the MechaCar_mpg.csv file as a dataframe.
-MechaCar_mpg <-read.csv(file = 'MechaCar_mpg.csv',check.names = F,stringsAsFactors =F)
+MechaCar_mpg <-read.csv(file='desktop/class/MechaCar_Statistical_analysis/MechaCar_mpg.csv',check.names = F,stringsAsFactors =F)
 
 ## Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), and add the dataframe you created in Step 4 as the data parameter.
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg)
